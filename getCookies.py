674ed4fe8fec3b1a32a7a7f11dbde23694107366
CULL4 <cullan.nichols@gmail.comgit config --global user.email>
2023-07-11 01:32:00 -0600

    Changed time before close window

--- getCookies.py
+++ getCookies.py
@@ -8,6 +8,6 @@ driver.get("https://buff.163.com/goods/857578")
 
 # Here, you must login to steam from the automated browser. Wait for the browser to exit and the cookies will be saved in the specified location
 selenium_cookie_file = 'selenium_cookie_file.txt'
-time.sleep(45) # wait for manual login, save cookies
+time.sleep(50) # wait for manual login, save cookies
 pickle.dump(pickle.dump(driver.get_cookies() , open("cookies.pkl","wb")))
 driver.quit()
